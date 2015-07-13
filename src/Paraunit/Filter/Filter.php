27f5c1edc2f06587d2589a35a62ceb45827f5299
Francesco Panina <francesco.panina@facile.it>
2015-07-13 16:03:28 +0200

    [Coverage]
     - fix cli.

--- src/Paraunit/Filter/Filter.php
+++ src/Paraunit/Filter/Filter.php
@@ -13,7 +13,7 @@ class Filter
 
     /**
      * @param string $configFile
-     * @param $testsuite
+     * @param        $testsuite
      * @return array
      */
     public function filterTestFiles($configFile, $testsuite)
@@ -25,17 +25,29 @@ class Filter
             ->getTestSuiteConfiguration($testsuite)
             ->getIterator();
 
+
         /** @var \PHPUnit_Framework_TestSuite $testSuite */
         foreach ($iterator as $testSuite) {
+
             $tests = $testSuite->getIterator();
 
             foreach ($tests as $test) {
-                $class = new \ReflectionClass($test);
-                $files[] = $class->getFileName();
+
+                if ($test instanceof \PHPUnit_Framework_TestSuite_DataProvider) {
+
+                    $actualTests = $test->tests();
+                    foreach ($actualTests as $actualTest) {
+                        $class   = new \ReflectionClass($actualTest);
+                        $files[] = $class->getFileName();
+                    }
+                } else {
+                    $class   = new \ReflectionClass($test);
+                    $files[] = $class->getFileName();
+                }
             }
         }
 
-        return $files;
+        return array_unique($files);
 
     }
 }
