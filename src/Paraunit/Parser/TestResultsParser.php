299a2d1c9363e021dd09382c5e7f35be1ab1e40e
Alessandro Lai <alessandro.lai@facile.it>
2015-07-13 16:12:03 +0200

    Fixed parsing for various PHPUnit versions

--- src/Paraunit/Parser/TestResultsParser.php
+++ src/Paraunit/Parser/TestResultsParser.php
@@ -7,8 +7,9 @@ use Paraunit\Process\ProcessResultInterface;
 
 class TestResultsParser implements ProcessOutputParserChainElementInterface
 {
-    const PHPUNIT_TEST_RESULTS_REGEX = '/(?<=dist\n\n)(.*?)+(?=\s+Time)/s';
+    const PHPUNIT_TEST_RESULTS_REGEX = '/(?<=by Sebastian Bergmann and contributors.\n\n)(.*?)+(?=\n\nTime:)/s';
     const PHPUNIT_TEST_SINGLE_RESULTS_REGEX = '/[FIES.]+/';
+    const PHPUNIT_CONFIG_LOAD = 'Configuration read from ';
 
     /**
      * @param ProcessResultInterface $process
@@ -16,14 +17,36 @@ class TestResultsParser implements ProcessOutputParserChainElementInterface
      */
     public function parseAndContinue(ProcessResultInterface $process)
     {
-        $results = array();
-        preg_match(self::PHPUNIT_TEST_RESULTS_REGEX, $process->getOutput(), $results);
+        $results = $this->getResultsFromOutput($process->getOutput());
 
-        if (!empty($results)) {
-            $cleanResults = preg_replace('/[^FIES.]+/', '', $results[0]);
+        if ($results != '') {
+            $cleanResults = preg_replace('/[^FIES.]+/', '', $results);
             $process->setTestResults(str_split($cleanResults));
         }
 
         return true;
+    }
+
+    /**
+     * @param string $output
+     * @return string
+     */
+    private function getResultsFromOutput($output)
+    {
+        $matches = [];
+        preg_match(self::PHPUNIT_TEST_RESULTS_REGEX, $output, $matches);
+
+        $results = '';
+
+        if (!empty($matches)) {
+            // fix per PHPUNIT < 4.7, strip della riga della config
+            if (substr($matches[0], 0, 24) == $this::PHPUNIT_CONFIG_LOAD) {
+                $results = preg_replace('/^'.preg_quote($this::PHPUNIT_CONFIG_LOAD).'.*\n\n/', '', $matches[0]);
+            } else {
+                $results = $matches[0];
+            }
+        }
+
+        return $results;
     }
 }
