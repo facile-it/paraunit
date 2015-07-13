ef43b194b04580a96b1646514b934ada8e582b8e
Francesco Panina <francesco.panina@facile.it>
2015-07-13 12:46:16 +0200

    [Coverage]
     - fix cli

--- Paraunit.php
+++ Paraunit.php
@@ -1,20 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Symfony\Component\Console\Application;
-use Paraunit\Command\ParallelCommand;
 
-require 'Container.php';
-
-$loader->load('services.yml');
-
-$command = new ParallelCommand(
-    $container->get('facile.cbr.parallel_test_bundle.filter.filter'),
-    $container->get('facile.cbr.parallel_test_bundle.runner.runner')
-);
-
-$application = new Application('Paraunit', '0.4');
-$application->add($command);
-$application->run();
 
 
