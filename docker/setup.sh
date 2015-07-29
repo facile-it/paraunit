9b354bdb4b1ce158dbc962fc1a937af49f548e27
ranpafin <francesco.panina@gmail.com>
2015-07-29 12:01:41 -0700

    Docker
     - Polished dockerfile.
     - Removed start script

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,7 +5,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 docker build -t paraunit_image $DIR
 
 docker rm -f paraunit_container
-docker run -d -v $DIR/../:/home/paraunit/projects --name paraunit_container -ti paraunit_image
+docker run -d -v $DIR/../:/home/paraunit/projects --name paraunit_container -ti paraunit_image bash
 
 sleep 1
 
