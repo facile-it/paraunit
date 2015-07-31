24159dc1d74d349602ba9a5b42dd80500114cbd6
ranpafin <francesco.panina@gmail.com>
2015-07-31 05:01:35 -0700

    Test sigsegv travis
     - 6th try to make it work on travis.

--- .travis/sigsegv-extension/build-for-travis.sh
+++ .travis/sigsegv-extension/build-for-travis.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd home/travis/build/facile-it/paraunit/.travis/sigsegv-extension
+cd /home/travis/build/facile-it/paraunit/.travis/sigsegv-extension
 
 phpize
 ./configure --enable-sigsegv
 make -j
 
-phpenv config-add ./home/travis/build/facile-it/paraunit/.travis/sigsegv.ini
+phpenv config-add /home/travis/build/facile-it/paraunit/.travis/sigsegv.ini
 
 cd home/travis/build/facile-it/paraunit/
