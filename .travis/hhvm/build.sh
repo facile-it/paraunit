a62a4748b1b0c42856461f1099efd2550df88be0
Lorenzo Fontana <fontanalorenzo@me.com>
2015-10-06 21:37:19 +0200

    Install modules

--- .travis/hhvm/build.sh
+++ .travis/hhvm/build.sh
@@ -5,5 +5,6 @@ cd /home/travis/build/facile-it/paraunit/.travis/hhvm
 hphpize
 cmake .
 make -j
+make install
 
 echo "hhvm.dynamic_extensions[sigsegv] = sigsegv.so" > /etc/hhvm/php.ini
--- .travis/php/sigsegv-extension/build-for-travis.sh
+++ .travis/php/sigsegv-extension/build-for-travis.sh
@@ -5,6 +5,7 @@ cd /home/travis/build/facile-it/paraunit/.travis/php/sigsegv-extension
 phpize
 ./configure --enable-sigsegv
 make -j
+make install
 
 phpenv config-add /home/travis/build/facile-it/paraunit/.travis/php/sigsegv.ini
 
