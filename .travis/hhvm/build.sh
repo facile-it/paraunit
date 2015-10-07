a7966a7060137297ad37918dec59be7f8e887d38
Lorenzo Fontana <fontanalorenzo@me.com>
2015-10-07 13:10:21 +0200

    Boost libraries

--- .travis/hhvm/build.sh
+++ .travis/hhvm/build.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-sudo apt-get -qq install g++-4.8 hhvm-dev
+sudo apt-get -qq install g++-4.8 hhvm-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-thread-dev libboost-context-dev
 export CXX="g++-4.8" CC="gcc-4.8"
 
 cd /home/travis/build/facile-it/paraunit/.travis/hhvm
