082379a790994504531cf5975a1b3064c34b87bd
Alessandro Lai <alessandro.lai85@gmail.com>
2015-12-08 17:10:57 +0100

    Removing ::class, we still support 5.3, damn!

--- src/Paraunit/Tests/Unit/Process/ProcessFactoryTest.php
+++ src/Paraunit/Tests/Unit/Process/ProcessFactoryTest.php
@@ -15,10 +15,10 @@ class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testCreateProcess()
     {
-        $phpUnitBin = $this->prophesize(PHPUnitBinFile::class);
+        $phpUnitBin = $this->prophesize('Paraunit\Configuration\PHPUnitBinFile');
         $phpUnitBin->getPhpUnitBin()->shouldBeCalled()->willReturn('phpunit');
 
-        $phpUnitConfigFile = $this->prophesize(PHPUnitConfigFile::class);
+        $phpUnitConfigFile = $this->prophesize('Paraunit\Configuration\PHPUnitConfigFile');
         $phpUnitConfigFile->getFileFullPath()->shouldBeCalled()->willReturn('configFile.xml');
 
         $factory = new ProcessFactory($phpUnitBin->reveal());
@@ -26,17 +26,17 @@ class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
 
         $process = $factory->createProcess("TestTest.php");
 
-        $this->assertInstanceOf(ParaunitProcessAbstract::class, $process);
+        $this->assertInstanceOf('Paraunit\Process\ParaunitProcessAbstract', $process);
         $this->assertEquals('phpunit -c configFile.xml --colors=never TestTest.php 2>&1', $process->getCommandLine());
     }
 
     public function testCreateProcessThrowsExceptionIfConfigIsMissing()
     {
-        $phpUnitBin = $this->prophesize(PHPUnitBinFile::class);
+        $phpUnitBin = $this->prophesize('Paraunit\Configuration\PHPUnitBinFile');
 
         $factory = new ProcessFactory($phpUnitBin->reveal());
 
-        $this->setExpectedException(\Exception::class, 'config missing');
+        $this->setExpectedException('\Exception', 'config missing');
         $factory->createProcess('test');
     }
 }
