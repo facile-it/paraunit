610cb931300d8f777ecfd00a74a435cec3b3c8d0
Alessandro Lai <alessandro.lai85@gmail.com>
2016-03-20 19:40:48 +0100

    Fix travis builds: cannot remove xDebug

--- src/Paraunit/Coverage/CoverageFetcher.php
+++ src/Paraunit/Coverage/CoverageFetcher.php
@@ -40,6 +40,6 @@ class CoverageFetcher
             return $codeCoverage;
         }
 
-        return new \PHP_CodeCoverage(new CodeCoverageStubDriver());
+        return new \PHP_CodeCoverage();
     }
 }
