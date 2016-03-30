3cdae5d9601fba8a94670d1f8d5bca37757b848f
Alessandro Lai <alessandro.lai@facile.it>
2016-03-30 15:09:22 +0200

    Switching to PSR2 standard

--- contributing.sh
+++ contributing.sh
@@ -2,7 +2,10 @@
 
 echo "Paraunit: Tidyng up the code..."
 
-phpcs -p --standard=phpcs.ruleset.xml --colors  src/
-phpcbf --standard=phpcs.ruleset.xml src/
+phpcs -p --standard=PSR2 --colors  src/
+phpcbf --standard=PSR2 src/
+
+phpcs -p --standard=PSR2 --colors  tests/
+phpcbf --standard=PSR2 tests/
 
 echo "Paraunit: DONE! You can now submit you PR!"
