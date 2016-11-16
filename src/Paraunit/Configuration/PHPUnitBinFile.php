615b1477c2a0f7baec8ec542481c48e0b3b767ad
Alessandro Lai <alessandro.lai85@gmail.com>
2016-11-16 12:59:06 +0100

    Build a PHAR with PHPUnit inside

--- src/Paraunit/Configuration/PHPUnitBinFile.php
+++ src/Paraunit/Configuration/PHPUnitBinFile.php
@@ -12,28 +12,37 @@ class PHPUnitBinFile
     const PHPUNIT_RELPATH_FOR_VENDOR = '/../../../../../phpunit/phpunit/phpunit';
     // I'm using Paraunit standalone (developing)
     const PHPUNIT_RELPATH_FOR_STANDALONE = '/../../../vendor/phpunit/phpunit/phpunit';
-    // I'm using Paraunit in a PHAR
-    const PHPUNIT_RELPATH_FOR_PHAR = 'vendor/phpunit/phpunit/phpunit';
 
     /** @var string Realpath to PHPUnit bin location */
     private $phpUnitBin;
 
     /**
      * PHPUnitBinFile constructor.
+     * @throws \RuntimeException
      */
     public function __construct()
     {
-        if (file_exists(__DIR__.self::PHPUNIT_RELPATH_FOR_VENDOR)) {
-            $this->phpUnitBin = __DIR__.self::PHPUNIT_RELPATH_FOR_VENDOR;
-        } elseif (file_exists(__DIR__.self::PHPUNIT_RELPATH_FOR_STANDALONE)) {
-            $this->phpUnitBin = __DIR__.self::PHPUNIT_RELPATH_FOR_STANDALONE;
-        } elseif (file_exists(self::PHPUNIT_RELPATH_FOR_PHAR)) {
-            $this->phpUnitBin = self::PHPUNIT_RELPATH_FOR_PHAR;
-        } else {
-            throw new \Exception('PHPUnit bin not found');
+        if (defined('PARAUNIT_PHAR_FILE')) {
+            // Paraunit is running as a standalone PHAR archive
+            // PHPUnit is embedded in the archive, self execute it in special mode
+            $this->phpUnitBin = PARAUNIT_PHAR_FILE . ' phpunit';
+
+            return;
         }
 
-        $this->phpUnitBin = realpath($this->phpUnitBin);
+        if (file_exists(__DIR__ . self::PHPUNIT_RELPATH_FOR_VENDOR)) {
+            $this->phpUnitBin = realpath(__DIR__ . self::PHPUNIT_RELPATH_FOR_VENDOR);
+
+            return;
+        }
+
+        if (file_exists(__DIR__ . self::PHPUNIT_RELPATH_FOR_STANDALONE)) {
+            $this->phpUnitBin = realpath(__DIR__ . self::PHPUNIT_RELPATH_FOR_STANDALONE);
+
+            return;
+        }
+
+        throw new \RuntimeException('PHPUnit bin not found');
     }
 
     /**
