2a7fb0b4c22e7f647371a2124c5ace7376c1c41f
Alessandro Lai <alessandro.lai85@gmail.com>
2016-11-24 13:27:05 +0100

    Test for PHPDbg only on PHP >= 7

--- tests/Functional/Configuration/PHPDbgBinFileTest.php
+++ tests/Functional/Configuration/PHPDbgBinFileTest.php
@@ -12,12 +12,28 @@ use Tests\BaseFunctionalTestCase;
  */
 class PHPDbgBinFileTest extends BaseFunctionalTestCase
 {
-    public function testGetPhpDbgBin()
+    public function testIsAvailable()
     {
         $bin = new PHPDbgBinFile();
-        
+
+        $this->assertSame($this->shouldPHPDbgDriverBeAvailable(), $bin->isAvailable());
+    }
+
+    public function testGetPhpDbgBin()
+    {
+        if (! $this->shouldPHPDbgDriverBeAvailable()) {
+            $this->markTestSkipped('PHPDbg coverage driver not available for PHP < 7.0');
+        }
+
+        $bin = new PHPDbgBinFile();
+
         $this->assertStringEndsWith('phpdbg', $bin->getPhpDbgBin());
         $this->assertNotContains(' ', $bin->getPhpDbgBin());
         $this->assertNotContains("\n", $bin->getPhpDbgBin());
+    }
+
+    private function shouldPHPDbgDriverBeAvailable()
+    {
+        return version_compare('7', PHP_VERSION, '<');
     }
 }
