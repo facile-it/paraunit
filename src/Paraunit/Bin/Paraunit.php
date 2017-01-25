0e8a023ed490351635931566633f36d998ca74cf
Alessandro Lai <alessandro.lai85@gmail.com>
2017-01-25 11:11:24 +0100

    Mark version as 0.8.1 [skip CI]

--- src/Paraunit/Bin/Paraunit.php
+++ src/Paraunit/Bin/Paraunit.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Application;
  */
 class Paraunit
 {
-    const VERSION = '0.8';
+    const VERSION = '0.8.1';
 
     public static function createApplication()
     {
