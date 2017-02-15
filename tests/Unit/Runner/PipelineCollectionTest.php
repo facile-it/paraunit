8e3cc6f04d9c7f44e9afd3bc0bfdebc2292288d1
Alessandro Lai <alessandro.lai85@gmail.com>
2017-02-15 11:06:54 +0100

    Enforce time limit on CI

--- tests/Unit/Runner/PipelineCollectionTest.php
+++ tests/Unit/Runner/PipelineCollectionTest.php
@@ -2,24 +2,63 @@
 
 namespace Tests\Unit\Runner;
 
+use Paraunit\Runner\Pipeline;
 use Paraunit\Runner\PipelineCollection;
+use Paraunit\Runner\PipelineFactory;
 use Tests\BaseUnitTestCase;
+use Tests\Stub\StubbedParaunitProcess;
 
 /**
  * Class PipelineCollectionTest
  * @package Tests\Unit\Runner
+ * @small
  */
 class PipelineCollectionTest extends BaseUnitTestCase
 {
     public function testInstantiation()
     {
+        $pipelines = array(
+            $this->prophesize('Paraunit\Runner\Pipeline')->reveal(),
+            $this->prophesize('Paraunit\Runner\Pipeline')->reveal(),
+            $this->prophesize('Paraunit\Runner\Pipeline')->reveal(),
+            $this->prophesize('Paraunit\Runner\Pipeline')->reveal(),
+            $this->prophesize('Paraunit\Runner\Pipeline')->reveal(),
+        );
+
+        new PipelineCollection($this->mockPipelineFactory($pipelines), count($pipelines));
+    }
+
+    public function testPush()
+    {
+        $process = new StubbedParaunitProcess();
+        $pipeline = $this->prophesize('Paraunit\Runner\Pipeline');
+        $pipeline->isFree()
+            ->willReturn(false);
+        $pipeline->isTerminated()
+            ->shouldBeCalledTimes(3)
+            ->willReturn(false, false, true);
+        $pipeline->execute($process)
+            ->shouldBeCalledTimes(1);
+
+        $collection = new PipelineCollection($this->mockPipelineFactory(array($pipeline->reveal())), 1);
+
+        $collection->push($process);
+    }
+
+    /**
+     * @param array | Pipeline[] $pipelines
+     * @return PipelineFactory
+     */
+    private function mockPipelineFactory(array $pipelines)
+    {
         $factory = $this->prophesize('Paraunit\Runner\PipelineFactory');
-        for ($i = 0; $i < 5; $i++) {
-            $factory->create($i)
+
+        foreach ($pipelines as $number => $pipeline) {
+            $factory->create($number)
                 ->shouldBeCalledTimes(1)
-                ->willReturn($this->prophesize('Paraunit\Runner\Pipeline')->reveal());
+                ->willReturn($pipeline);
         }
-        
-        new PipelineCollection($factory->reveal(), 5);
+
+        return $factory->reveal();
     }
 }
