919d447533240a9486375ffa994dd0b047d9caa0
Alessandro Lai <alessandro.lai85@gmail.com>
2017-02-15 11:29:05 +0100

    Complete PipelineCollection unit test

--- tests/Unit/Runner/PipelineCollectionTest.php
+++ tests/Unit/Runner/PipelineCollectionTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit\Runner;
 use Paraunit\Runner\Pipeline;
 use Paraunit\Runner\PipelineCollection;
 use Paraunit\Runner\PipelineFactory;
+use Prophecy\Argument;
+use Symfony\Bridge\PhpUnit\ClockMock;
 use Tests\BaseUnitTestCase;
 use Tests\Stub\StubbedParaunitProcess;
 
@@ -30,19 +32,37 @@ class PipelineCollectionTest extends BaseUnitTestCase
 
     public function testPush()
     {
-        $process = new StubbedParaunitProcess();
         $pipeline = $this->prophesize('Paraunit\Runner\Pipeline');
         $pipeline->isFree()
             ->willReturn(false);
         $pipeline->isTerminated()
             ->shouldBeCalledTimes(3)
             ->willReturn(false, false, true);
-        $pipeline->execute($process)
+        $pipeline->execute(Argument::cetera())
             ->shouldBeCalledTimes(1);
 
         $collection = new PipelineCollection($this->mockPipelineFactory(array($pipeline->reveal())), 1);
 
-        $collection->push($process);
+        $collection->push(new StubbedParaunitProcess());
+
+        ClockMock::withClockMock(false);
+    }
+
+    public function testWaitCompletion()
+    {
+        $freePipeline = $this->prophesize('Paraunit\Runner\Pipeline');
+        $freePipeline->isFree()
+            ->shouldBeCalled()
+            ->willReturn(true);
+        $pipeline = $this->prophesize('Paraunit\Runner\Pipeline');
+        $pipeline->isFree()
+            ->willReturn(false);
+        $pipeline->waitCompletion()
+            ->willReturn(new StubbedParaunitProcess());
+
+        $collection = new PipelineCollection($this->mockPipelineFactory(array($pipeline->reveal(), $freePipeline->reveal())), 2);
+
+        $collection->waitForCompletion();
     }
 
     /**
@@ -60,5 +80,20 @@ class PipelineCollectionTest extends BaseUnitTestCase
         }
 
         return $factory->reveal();
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        ClockMock::register('Paraunit\Runner\PipelineCollection');
+        ClockMock::withClockMock(true);
+    }
+
+    protected function tearDown()
+    {
+        ClockMock::withClockMock(false);
+
+        parent::tearDown();
     }
 }
