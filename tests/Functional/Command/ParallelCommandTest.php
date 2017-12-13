cb50a9f5b180656ef38df58443ce4f0ccb8c4e21
Alessandro Lai <alessandro.lai85@gmail.com>
2017-12-13 11:25:54 +0100

    Revert a wrong edit and realign a few tests

--- tests/Functional/Command/ParallelCommandTest.php
+++ tests/Functional/Command/ParallelCommandTest.php
@@ -87,7 +87,7 @@ class ParallelCommandTest extends BaseTestCase
         $this->assertContains(MySQLDeadLockTestStub::class, $output);
         $this->assertNotEquals(0, $exitCode);
 
-        $this->assertContains('Executed: 11 test classes, 30 tests (12 retried)', $output);
+        $this->assertContains('Executed: 12 test classes (15 retried), 22 tests', $output);
     }
 
     public function testExecutionWithLogo()
@@ -125,12 +125,18 @@ class ParallelCommandTest extends BaseTestCase
         $output = $commandTester->getDisplay();
         $this->assertNotEquals(0, $exitCode);
 
-        $this->assertContains('Executed: 11 test classes, 30 tests (12 retried)', $output, 'Precondition failed');
-        $processesCount = 11 + 12;
+        $classExecuted = 12;
+        $processRetried = 15;
+        $processesCount = $classExecuted + $processRetried;
+        $this->assertContains(
+            sprintf('Executed: %d test classes (%d retried), 22 tests', $classExecuted, $processRetried), 
+            $output, 
+            'Precondition failed'
+        );
         $this->assertSame($processesCount, substr_count($output, 'PROCESS STARTED'));
         $this->assertSame($processesCount, substr_count($output, 'PROCESS TERMINATED'));
-        $this->assertSame($processesCount, substr_count($output, 'PROCESS PARSING COMPLETED'));
-        $this->assertSame(12, substr_count($output, 'PROCESS TO BE RETRIED'));
+        $this->assertSame($classExecuted, substr_count($output, 'PROCESS PARSING COMPLETED'));
+        $this->assertSame($processRetried, substr_count($output, 'PROCESS TO BE RETRIED'));
     }
 
     public function testExecutionWithoutConfiguration()
@@ -165,7 +171,7 @@ class ParallelCommandTest extends BaseTestCase
 
         $output = $commandTester->getDisplay();
         $this->assertNotEquals(0, $exitCode);
-        $this->assertContains('Executed: 1 test classes, 3 tests (0 retried)', $output, 'Precondition failed');
+        $this->assertContains('Executed: 1 test classes, 3 tests', $output, 'Precondition failed');
         $this->assertContains('1 files with DEPRECATION WARNINGS:', $output);
         $this->assertContains(RaisingDeprecationTestStub::DEPRECATION_MESSAGE, $output);
         $this->assertContains('RaisingDeprecationTestStub::testDeprecation', $output);
