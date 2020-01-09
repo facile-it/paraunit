facile-it/paraunit

commit 9aa8b199db5a6fbc0ebf8681bcb3ee327aa46929

Wrap BC layer in conditional to avoid deprecation

src/Process/ProcessFactory.php
@@ -40,7 +40,10 @@ class ProcessFactory implements ProcessFactoryInterface
             $this->environmentVariables
         );
 
-        $process->inheritEnvironmentVariables();
+        if (class_exists(Symfony\Component\Process\ProcessBuilder::class)) {
+            // Symfony 3.4 BC layer
+            $process->inheritEnvironmentVariables();
+        }
 
         return new SymfonyProcessWrapper($process, $testFilePath);
     }

commit e92a961bc998bd0c2ba466d3849ec18acce52335

Fix bug in BC layer introduced in 9aa8b199db5a6fbc0ebf8681bcb3ee327aa46929

src/Process/ProcessFactory.php
@@ -40,7 +40,7 @@ class ProcessFactory implements ProcessFactoryInterface
             $this->environmentVariables
         );
 
-        if (class_exists(Symfony\Component\Process\ProcessBuilder::class)) {
+        if (class_exists(\Symfony\Component\Process\ProcessBuilder::class)) {
             // Symfony 3.4 BC layer
             $process->inheritEnvironmentVariables();
         }