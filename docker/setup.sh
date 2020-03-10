d74075f5021fc8bb86a97a140dde35282dbf1e5e
Alessandro Lai <alessandro.lai85@gmail.com>
2020-03-10 09:09:34 +0100

    Require PHPunit 9.1 with fork and PHP 7.3

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,4 +5,4 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 docker build -t paraunit_container $DIR
-docker run -v $DIR/../:/home/paraunit/projects --rm -ti -u paraunit paraunit_container zsh
+docker run -v $DIR/../:/home/paraunit/projects -v ~/.composer:/home/paraunit/.composer --rm -ti -u paraunit paraunit_container zsh
