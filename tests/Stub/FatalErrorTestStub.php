4128728d1075a25ac02e49cbe8deeae43c3e8c1a
Alessandro Lai <alessandro.lai85@gmail.com>
2021-10-08 22:59:18 +0200

    Avoid PHP 8.1 deprecation

--- tests/Stub/FatalErrorTestStub.php
+++ tests/Stub/FatalErrorTestStub.php
@@ -8,7 +8,7 @@ class FatalErrorTestStub extends BrokenTestBase implements BrokenTestInterface
 {
     public function testBrokenTest(): void
     {
-        $foo = new class() implements \Serializable {
+        $foo = new class() implements \JsonSerializable {
         };
 
         $message = 'This assertion should not happen: ' . json_encode($foo);
