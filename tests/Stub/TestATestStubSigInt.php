790096035b5368257cceff3cffc5f2a2a9bae768
Przemysław Czerkas <pczerkas@gmail.com>
2022-05-13 22:18:59 +0200

    Change sleep

--- tests/Stub/TestATestStubSigInt.php
+++ tests/Stub/TestATestStubSigInt.php
@@ -8,7 +8,7 @@ class TestATestStubSigInt extends BrokenTestBase implements BrokenTestInterface
 {
     public function testBrokenTest(): void
     {
-        usleep(100000);
+        usleep(500000);
 
         $chunkFileName = __DIR__ . DIRECTORY_SEPARATOR . 'phpunit_for_sigint_stubs_0.xml';
 
