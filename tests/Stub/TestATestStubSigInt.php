6586ff1bc7c8bda38831ab1a177cd467d0fbdc0b
Przemysław Czerkas <pczerkas@gmail.com>
2022-05-16 18:49:16 +0200

    Give parent process some time to react

--- tests/Stub/TestATestStubSigInt.php
+++ tests/Stub/TestATestStubSigInt.php
@@ -8,13 +8,20 @@ class TestATestStubSigInt extends BrokenTestBase implements BrokenTestInterface
 {
     public function testBrokenTest(): void
     {
-        usleep(200000);
-
         $chunkFileName = __DIR__ . DIRECTORY_SEPARATOR . 'phpunit_for_sigint_stubs_0.xml';
 
         $this->assertFileExists($chunkFileName);
+
         posix_kill(posix_getppid(), SIGINT);
-        usleep(200000);
+
+        // give parent process 5s to react
+        for ($i = 1; $i <= 100; $i++) {
+            if (!file_exists($chunkFileName)) {
+                break;
+            }
+            usleep(50000);
+        }
+
         $this->assertFileDoesNotExist($chunkFileName);
     }
 }
