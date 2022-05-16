2e4cd81ae6f689ea192fb50d0bdc2a49e57c5590
Przemysław Czerkas <pczerkas@gmail.com>
2022-05-16 17:05:26 +0000

    Wait some time for other chunk to finish

--- tests/Stub/TestATestStubSigInt.php
+++ tests/Stub/TestATestStubSigInt.php
@@ -9,6 +9,15 @@ class TestATestStubSigInt extends BrokenTestBase implements BrokenTestInterface
     public function testBrokenTest(): void
     {
         $chunkFileName = __DIR__ . DIRECTORY_SEPARATOR . 'phpunit_for_sigint_stubs_0.xml';
+        $otherChunkFileName = __DIR__ . DIRECTORY_SEPARATOR . 'phpunit_for_sigint_stubs_1.xml';
+
+        // wait 5s for other chunk to finish
+        for ($i = 1; $i <= 100; ++$i) {
+            usleep(50000);
+            if (! file_exists($otherChunkFileName)) {
+                break;
+            }
+        }
 
         $this->assertFileExists($chunkFileName);
 
