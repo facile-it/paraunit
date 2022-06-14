6f3fc2e6d702037f4d72e65218a3e7a4e9a0550b
Alessandro Lai <alessandro.lai85@gmail.com>
2022-06-14 16:32:54 +0200

    Add Xdebug 3 mode handling

--- src/Proxy/XDebugProxy.php
+++ src/Proxy/XDebugProxy.php
@@ -10,4 +10,16 @@ class XDebugProxy
     {
         return extension_loaded('xdebug');
     }
+
+    /**
+     * @psalm-return 2|3
+     */
+    public function getMajorVersion(): int
+    {
+        if (version_compare('3.0', phpversion('xdebug'), '>=')) {
+            return 3;
+        }
+
+        return 2;
+    }
 }
