742ad7e9a3be23397b42e8fbe1d15d64f69b3764
Alessandro Lai <alessandro.lai85@gmail.com>
2024-03-18 08:50:06 +0100

    Upgrade Rector config

--- rector.php
+++ rector.php
@@ -2,40 +2,29 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->parallel();
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withImportNames(
+        importShortClasses: false
+    )
+    ->withParallel()
+    ->withPaths([
         __FILE__,
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::PSR_4,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    // skip rules
-    $rectorConfig->skip([
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,  
+    )
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_100,
+    ])
+    ->withSkip([
         ReturnNeverTypeRector::class,
-        AddDoesNotPerformAssertionToNonAssertingTestRector::class => __DIR__ . '/tests/Stub', 
     ]);
-};
