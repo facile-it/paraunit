0ebcf8028414f4c8390988b5da3b1b9eb51f8184
Alessandro Lai <alessandro.lai85@gmail.com>
2024-08-20 16:42:23 +0200

    Fix CS

--- tests/Stub/FatalErrorTestStub.php
+++ tests/Stub/FatalErrorTestStub.php
@@ -8,7 +8,7 @@ class FatalErrorTestStub extends BrokenTestBase implements BrokenTestInterface
 {
     public function testBrokenTest(): void
     {
-        $foo = new class () implements \JsonSerializable {};
+        $foo = new class implements \JsonSerializable {};
 
         $message = 'This assertion should not happen: ' . json_encode($foo, JSON_THROW_ON_ERROR);
 
