7dab2deb72a28d34920f700986ce388b106924d6
Alessandro Lai <alessandro.lai85@gmail.com>
2025-02-17 09:57:41 +0100

    Fix new Psalm issues

--- src/Configuration/ParallelConfiguration.php
+++ src/Configuration/ParallelConfiguration.php
@@ -68,6 +68,9 @@ class ParallelConfiguration
         }
     }
 
+    /**
+     * @psalm-suppress MixedArgument
+     */
     protected function loadCommandLineOptions(ContainerBuilder $containerBuilder, InputInterface $input): void
     {
         $containerBuilder->setParameter('paraunit.max_process_count', $input->getOption('parallel'));
