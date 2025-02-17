c8d87d4698857a800443222b3029623c2bc53260
Alessandro Lai <alessandro.lai85@gmail.com>
2025-02-17 17:59:49 +0100

    Fix CS

--- src/Printer/FinalPrinter.php
+++ src/Printer/FinalPrinter.php
@@ -83,7 +83,7 @@ class FinalPrinter implements EventSubscriberInterface
     {
         $this->output->writeln('');
         $this->output->writeln('');
-        $this->output->writeln('Execution time -- ' . gmdate('H:i:s', (int) ((int)$stopEvent->getDuration() / 1_000)));
+        $this->output->writeln('Execution time -- ' . gmdate('H:i:s', (int) ((int) $stopEvent->getDuration() / 1_000)));
     }
 
     private function printTestCounters(): void
