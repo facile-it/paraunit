20c420e5bb4b3eff447a47e86be7ce82d21fc4e3
Alessandro Lai <alessandro.lai85@gmail.com>
2026-04-07 22:55:03 +0200

    Update DI container configuration

--- src/Configuration/DependencyInjection/CoverageContainerDefinition.php
+++ src/Configuration/DependencyInjection/CoverageContainerDefinition.php
@@ -8,11 +8,13 @@ use Paraunit\Configuration\PHPDbgBinFile;
 use Paraunit\Coverage\CoverageFetcher;
 use Paraunit\Coverage\CoverageMerger;
 use Paraunit\Coverage\CoverageResult;
+use Paraunit\Coverage\V14;
 use Paraunit\Printer\CoveragePrinter;
 use Paraunit\Process\CommandLineWithCoverage;
 use Paraunit\Process\ProcessFactory;
 use Paraunit\Proxy\PcovProxy;
 use Paraunit\Proxy\XDebugProxy;
+use SebastianBergmann\CodeCoverage\Node\Directory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
@@ -46,9 +48,18 @@ class CoverageContainerDefinition extends ParallelContainerDefinition
 
     private function configureCoverage(ContainerBuilder $container): void
     {
-        $container->autowire(CoverageFetcher::class);
-        $container->autowire(CoverageMerger::class);
-        $container->autowire(CoverageResult::class);
         $container->autowire(CoveragePrinter::class);
+
+        if (class_exists(Directory::class)) {
+            // PHPUnit 13.1+ with phpunit/php-code-coverage v14
+            $container->autowire(V14\CoverageFetcher::class);
+            $container->autowire(V14\CoverageMerger::class);
+            $container->autowire(V14\CoverageResult::class);
+        } else {
+            // PHPUnit 13.0 with phpunit/php-code-coverage v13 and below
+            $container->autowire(CoverageFetcher::class);
+            $container->autowire(CoverageMerger::class);
+            $container->autowire(CoverageResult::class);
+        }
     }
 }
